119e202cca09aa28a37ef4865f34a750fcdca66f
billyjack1988 <billyjacktattoos@gmail.com>
2017-08-28 14:13:10 -0500

    tested for 13 numbers

--- testing.rb
+++ testing.rb
@@ -7,8 +7,12 @@ class Ismb_Numbers < Minitest::Test
         assert_equal([], empty)
     end
 
-    def test_for_length
+    def test_for_length_10
         assert_equal(true, len("1234567890"))
+    end
+
+    def test_for_length_13
+        assert_equal(true, len("1234567890123"))\
     end
     
 end
