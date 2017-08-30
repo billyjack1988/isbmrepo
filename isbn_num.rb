325314b508c3b206751152e92e8a39a258648e7d
billyjack1988 <billyjacktattoos@gmail.com>
2017-08-30 08:35:55 -0500

    failed test for 13 sum

--- isbn_num.rb
+++ isbn_num.rb
@@ -71,11 +71,11 @@ end
 def isbn_array_with_13(isbn_code)
     13_isbn = isbn_code.gsub(/[^0-9]/, '')
         new_array = 13_isbn.split("").map(&:to_i)
-    isbn_num_sum_with_13(13_array)
+    isbn_num_sum_with_13(new_array)
 end
 
-def isbn_num_sum_with_13(13_array)
-    sum_13 = 13_array[0] * 1 + 13_array[1] * 3 + 13_array[2] * 1 + 13_array[3] * 3 + 13_array[4] * 1 + 13_array[5] * 3 + 13_array[6] * 1 + 13_array[7] * 3 + 13_array[8] * 1 + 13_array[9] * 3 + 13_array[10] * 1 + 13_array[11] * 3
+def isbn_num_sum_with_13(thr_array)
+    sum_13 = thr_array[0] * 1 + thr_array[1] * 3 + thr_array[2] * 1 + thr_array[3] * 3 + thr_array[4] * 1 + thr_array[5] * 3 + thr_array[6] * 1 + thr_array[7] * 3 + thr_array[8] * 1 + thr_array[9] * 3 + thr_array[10] * 1 + thr_array[11] * 3
     checksum = sum_13 % 10
     x = 10 - checksum
     final_sum = x % 10
