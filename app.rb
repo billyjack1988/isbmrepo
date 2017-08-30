221d51b2732082fe69be4f715bc2a4b177110acd
billyjack1988 <billyjacktattoos@gmail.com>
2017-08-30 12:31:21 -0500

    got the func to work for the 10

--- app.rb
+++ app.rb
@@ -10,12 +10,23 @@ post "/isbn_numbers" do
     isbn_ten = params[:ten]
     isbn_thr = params[:thr]
     # p "#{isbn_ten}this is ten isbn #{isbn_thr}this is thr isbn"
-   redirect'/results?ten='+ isbn_ten + '&thr=' + isbn_thr
+   redirect'/result?ten='+ isbn_ten + '&thr=' + isbn_thr
 end
 
-get '/results'
-    erb :results, locals:{ten: isbn_ten, thr: isbn_thr}
+get '/result' do
+    isbn_ten = params[:ten]
+    isbn_thr = params[:thr]  
+    isbn_fuck = isbn_array(isbn_ten)
+    p "#{isbn_fuck}this is after calling on func"
+    # erb :results, locals:{ten: isbn_ten, thr: isbn_thr}
+    # p "#{isbn_ten}this is ten isbn #{isbn_thr}this is thr isbn"
 end
+
+
+
+
+
+
 
 
 
